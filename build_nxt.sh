c423fa50432f1f9664ab7f3af1c727587b65d19f
mortyL <mortyl0834@gmail.com>
2023-01-11 12:09:13 +0800

    [CI][other]: adjust to main repo

--- build_nxt.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-docker run -it --privileged \
-    --env=LOCAL_USER_ID="$(id -u)" \
-    -v ~/workspace/NxtPX4/PX4-Autopilot:/src/PX4-Autopilot/:rw \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-    -e DISPLAY=:0 \
-    -p 14556:14556/udp \
-    --name=NxtCompileContainer px4io/px4-dev-nuttx-focal:2021-09-08 bash;
